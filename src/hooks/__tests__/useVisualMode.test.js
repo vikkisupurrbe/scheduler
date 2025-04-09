654a35394d5c2a85001dd25be169ee5314ee3926
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-08 20:31:31 -0700

    Add back limit and transition with replace

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -6,7 +6,7 @@ const FIRST = "FIRST";
 const SECOND = "SECOND";
 const THIRD = "THIRD"
 
-/*test("useVisualMode should initialize with default value", () => {
+test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   expect(result.current.mode).toBe(FIRST);
@@ -17,7 +17,7 @@ test("useVisualMode should transition to another mode", () => {
 
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
-});*/
+});
 
 test("useVisualMode should return to previous mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -34,3 +34,24 @@ test("useVisualMode should return to previous mode", () => {
   act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
 });
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+})
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,12 +6,24 @@ export default function useVisualMode(initial) {
   // Keep track of the history of the modes
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
-    setHistory(prev => [...prev, newMode])
-  }
+  const transition = (newMode, replace = false) => {
+    setHistory(prev => {
+      if (replace) {
+        return [...prev.slice(0, prev.length - 1), newMode]; // replace the last mode
+      } else {
+        return [...prev, newMode];
+      }
+    });
+  };
 
   const back = () => {
-    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    setHistory(prev => {
+      if (prev.length > 1) {
+        return prev.slice(0, prev.length - 1);
+      } else {
+        return prev; // Cannot remove the initial state
+      }
+    });
   }
 
   return { mode: history[history.length - 1], transition, back };
