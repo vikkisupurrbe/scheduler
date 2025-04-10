9027ff61e73bd1c1a432355f9e4556d1b93d7e71
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-09 19:42:17 -0700

    Add tests for form component

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -30,6 +30,7 @@ export default function Form(props) {
             onChange={event => {
               setName(event.target.value);
             }}
+            data-testid="student-name-input"
           />
         </form>
         <InterviewerList
