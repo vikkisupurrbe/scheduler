1c8a702aae3a890413d7acdc4ff482750882dc25
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-10 12:37:34 -0700

    Add and test mock axios library

--- src/reducers/application.test.js
+++ src/reducers/application.test.js
@@ -1,7 +1,27 @@
 import reducer from "./application";
+import '@testing-library/jest-dom';
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
+import Application from "../components/Application"
 
 describe("Application Reducer", () => {
   it("thows an error with an unsupported type", () => {
     expect(() => reducer({}, { type: null })).toThrow();
+  });
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { findByText } = render(<Application />);
+  
+    return findByText("Monday");
+  });
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { queryByText, findByText } = render(<Application />);
+  
+    return findByText("Monday").then(() => {
+      fireEvent.click(queryByText("Tuesday"));
+      expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 });
